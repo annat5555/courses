dfc9b5fa61ae3e6e94d48d66ec129a968437a05f
Anna Tsesar <anna5555@ukr.net>
2022-10-14 14:05:46 +0300

    Added comments to task4-1.php

--- task4-1.php
+++ task4-1.php
@@ -1,26 +1,31 @@
 <?php
+
+// class Employee - keeps data about employee's name and salary
 class Employee 
 {
     protected $name;
     protected $salary;
     
-    
+    // constructor which creates a new employee and allows to specify name and salary
     public function __construct( $n, $s )
     {
 		$this->name = $n;
 		$this->salary = $s;
 	}
     
+    // method which allows to obtain name of the employee
     function getName()
     {
 		return $this->name;
 	}
 
+	// method which allows to obtain salary of the employee
 	function getSalary()
     {
 		return $this->salary;
 	}
 	
+	// method which allows to double the salary of the employee
 	function doubleSalary()
     {
 		$this->salary *= 2;
@@ -29,29 +34,35 @@ class Employee
 	
 }
 
+
+// class Rectangle - keeps important data about such geometric shape as a rectangle
 class Rectangle 
 {
     protected $width;
     protected $height;
     
 
-
+	// constructor which creates a new rectangle and allows to specify its width and height
 	public function __construct( $w, $h )
     {
 		$this->width = $w;
 		$this->height = $h;
 	}
 	
+	// method which calculates square space of the rectangle
 	function getSquare()
     {
 		return $this->width *  $this->height;
 	}
 	
+	// method which calculates perimeter of the rectangle
 	function getPerimeter()
     {
 		return ( $this->width + $this->height) * 2;
 	}
 }
+
+
 
 //Create object
 $user1 = New Employee( "Kolya", 34800 ); //First object
